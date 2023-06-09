3623b58b4ef4df16b3243154057426c4514fb3bb
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-06-09 20:33:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerLeft() {
+    const left = positionToInteger(DODGER.style.left);
+    
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const left = positionToInteger(DODGER.style.left);
+    
+    if (left < GAME_WIDTH - DODGER_WIDTH) {
+      DODGER.style.left = `${left + 4}px`;
+    }
+  }
+  
+
+function positionToInteger(position) {
+  return parseInt(position.split('px')[0]) || 0;
+}
+
+const DODGER = document.getElementById('dodger');
+
+const GAME_WIDTH = 400;
+
+const DODGER_WIDTH = 40;
+
